6582e21ba5ed12949201ddafa0317d83ea23b033
Mircea Cadariu <m.cadariu@sig.eu>
2015-11-07 20:16:35 +0100

    Improved some formatting

--- gigi/src/test/java/com/learnakka/App.java
+++ gigi/src/test/java/com/learnakka/App.java
@@ -4,21 +4,18 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 
-
-public class App 
-{
-    @SuppressWarnings({"unused"})
-	public static void main( String[] args ) throws InterruptedException
-    {
-    	ActorSystem system = ActorSystem.create("system");
-    	ActorRef tokenizer = system.actorOf(Props.create(TokenizerActor.class), "tokenizer");
-    	ActorRef aggregator = system.actorOf(Props.create(AggregatingActor.class), "aggregator");
-    	ActorRef counting1 = system.actorOf(Props.create(CountingActor.class), "counting0");
+public class App {
+	@SuppressWarnings({ "unused" })
+	public static void main(String[] args) throws InterruptedException {
+		ActorSystem system = ActorSystem.create("system");
+		ActorRef tokenizer = system.actorOf(Props.create(TokenizerActor.class), "tokenizer");
+		ActorRef aggregator = system.actorOf(Props.create(AggregatingActor.class), "aggregator");
+		ActorRef counting1 = system.actorOf(Props.create(CountingActor.class), "counting0");
 		ActorRef counting2 = system.actorOf(Props.create(CountingActor.class), "counting1");
 		ActorRef counting3 = system.actorOf(Props.create(CountingActor.class), "counting2");
-    	tokenizer.tell("bla bla chi chi exe mmm ohh lala la", ActorRef.noSender());
-    	
-    	Thread.sleep(100);
-    	system.stop(aggregator);
-    }
+		tokenizer.tell("bla bla chi chi exe mmm ohh lala la", ActorRef.noSender());
+
+		Thread.sleep(100);
+		system.stop(aggregator);
+	}
 }
